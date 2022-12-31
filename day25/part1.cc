75b602e7dda7de833b0472c17e15b2560e22efa9
Josh Bennett <jbennett@anduril.com>
2022-12-30 23:19:20 -0800

    Snafu

---
+++ day25/part1.cc
@@ -0,0 +1,31 @@
+#include <atomic>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+int snafuToDecimal(const std::string& input) {
+  for (int i = 0; i < input.size(); ++i) {
+    char current = input[input.size() - 1 - i];
+  }
+}
+
+std::string decimalToSnafu(const int number) {}
+
+int main(int argc, char** argv) {
+  std::string line;
+  std::vector<std::string> lines;
+  while (std::getline(std::cin, line)) {
+    lines.push_back(line);
+  }
+  return 0;
+}
